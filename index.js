dea10b01de7926856fbf092adb61162921527236
AlexFrometa <you@example.com>
2021-06-09 16:40:55 -0500

    trying to submit this

--- index.js
+++ index.js
@@ -192,7 +192,7 @@ Use the getAverageWordLength function below to do the following:
     For example: getAverageWordLength(originalFlavors) should return a number between 0 and 3.     
 */
 
-function getAverageWordLength(/*code here*/){
+function getAverageWordLength(arr){
     /*code here*/
 }
 
